4cb403a39e3d96cee01d684c8db9cdc2b83d4b5f
Jacob Sartin <59787306+JacobSartin@users.noreply.github.com>
2025-12-03 15:58:11 -0600

    day 3

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-[Advent of Code 2025](https://adventofcode.com/2025)
+# [Advent of Code 2025](https://adventofcode.com/2025)
+
+each day has an input.txt that is excluded from this repository per the request of advent of code, this file is needed to generate an answer. It can be found on the website under the corresponding problem's page.
---
+++ day 3/puzzle_2.py
@@ -0,0 +1,30 @@
+banks: list[str] = []
+ans = 0
+# number fo batteries in a bank that can be used
+ACTIVE_BATTERIES = 12
+# Read in the input file, 1 bank per line
+with open("day 3/input.txt", "r") as f:
+    for line in f:
+        banks.append(line.strip())
+
+# main assumption: we can greedily search for the largest number for the furthest left digit
+# this will always result in the largest number, repeat for each following digit
+
+for index, bank in enumerate(banks):
+    # find max digit, index of max
+    max: list[int] = [0] * (ACTIVE_BATTERIES + 2)
+    joltage: str = ""
+
+    for i in range(1, ACTIVE_BATTERIES + 1):
+        # need to have batteries remaining for the following active slots
+        for j in range(max[i - 1] + 1, len(bank) - (ACTIVE_BATTERIES - i)):
+            if int(bank[j]) > int(bank[max[i]]):
+                max[i] = j
+                # 9 is max value, dont need to continue
+                if int(bank[j]) == 9:
+                    break
+        joltage += bank[max[i]]
+        max[i + 1] = max[i] + 1
+    ans += int(joltage)
+    print(f"Max joltage for bank {index} is {joltage}")
+print(f" ans: {ans}")
